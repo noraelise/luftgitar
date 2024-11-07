c7d9575ba5e0a3ee357b35436f92d03f82d8c476
noraelise <nora.elise@gmail.com>
2024-11-07 17:09:43 +0100

    Added global varable for guitar detection

--- logic.py
+++ logic.py
@@ -1,11 +1,11 @@
 
-def playing_air_guitar(data_points):
+def playing_air_guitar(data_points) -> bool:
     (right_elbow_angle, left_elbow_angle,
      dist_rhand_waist, dist_lhand_waist,
      height_right_wrist, height_left_wrist) = data_points
 
-    if ((30 < right_elbow_angle < 140) and (30 < left_elbow_angle < 180) and
-            (0 < dist_rhand_waist < 0.08) and (0 < dist_lhand_waist < 0.2) and
-            (0.35 < height_right_wrist < 0.65) and (0.3 < height_left_wrist < 0.63)):
+    if ((30 < right_elbow_angle < 140)      and (30 < left_elbow_angle < 180) and
+        (0 < dist_rhand_waist < 0.08)       and (0 < dist_lhand_waist < 0.2) and
+        (0.35 < height_right_wrist < 0.65)  and (0.3 < height_left_wrist < 0.63)):
         return True
     return False
